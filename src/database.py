b0b685b25a06d1a0c1b697a47fe7869a3112d801
Eyob Teshale <teeyob@abaybank.local>
2024-10-05 14:31:16 +0100

    Initial Commit

---
+++ src/database.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine
+from sqlalchemy.exc import OperationalError
+
+def get_connection():
+    hostname = 'localhost'
+    database = 'telecom_db'
+    username = 'postgres'
+    pwd = '2525'
+    port_id = '5432'
+
+    # Construct the database URL required by SQLAlchemy
+    db_url = f'postgresql+psycopg2://{username}:{pwd}@{hostname}:{port_id}/{database}'
+
+    try:
+        # Create an SQLAlchemy engine
+        engine = create_engine(db_url)
+        
+        # Test the connection by establishing it temporarily
+        with engine.connect() as connection:
+            print("Connection successful")
+        
+        # Return the engine to be used with pandas
+        return engine
+    except OperationalError as e:
+        print("OperationalError:", e)
+    except Exception as e:
+        print("Error connecting to the database:", e)
+    return None
+
